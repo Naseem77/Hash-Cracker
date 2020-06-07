24777855ae8dd2189d2ecbf62ab3b33f300c5377
aymanab5 <aymn-an@hotmail.com>
2020-06-07 16:29:11 +0300

    Update hashcracker.py

--- hashcracker.py
+++ hashcracker.py
@@ -4,36 +4,32 @@ from itertools import product
 from tkinter import *
 from tkinter import ttk
 
- 
-    
 
-def get_algorithm( type ):
-
-    def algorithm( string ):
-        h = type()
-        h.update(string.encode('utf-8'))
-        return h.hexdigest()
+def getAlgorithm(type):
+    def algorithm(string):
+        temp = type()
+        temp.update(string.encode('utf-8'))
+        return temp.hexdigest()
     return algorithm
 
 
-TYPES_DICT = { 32 : get_algorithm( hashlib.md5 ) }
+Dictionary_TYPES = { 32 : getAlgorithm( hashlib.md5 ),40 : getAlgorithm( hashlib.sha1 )}
 
 
 
-class Control( object ):
-    """ Main class """
+class Control(object):
 
     root = Tk(className='Hash Cracker')
     user_input = StringVar(root)
     enterHash = Entry(root,textvariable=user_input);
     myLabel1 = Label(root, text="");
-    decrypt_method = None
-    decrypted_hash = None
-    
-    def __init__( self ):
+    decryptedHash = None
+    inputFile = None
 
-        
-        # set window size
+    def __init__(self):
+
+        self.inputFile = None
+
         self.root.geometry("400x400")
         labelTitle = Label(self.root,text= "Hash Cracker",relief ="solid",
                                    width=20,font=("arial",19,"bold"))
@@ -61,68 +57,80 @@ class Control( object ):
         myButton.place(x=230,y=220)
 
         self.myLabel1.pack();
-        
+
         self.root.mainloop();
 
-        
-    
-    
+
+
     def myClick(self):
-        
+
         self.myLabel1.place(x=70,y=250)
-        self.user_hash = self.get_hash()
-        self.decrypted_hash = self.brute_force()
         self.myLabel1.config(text='Checking...')
-        self.elapsed = (time.time() - self.start)  
-     
-        self.myLabel1.config(text='Hash cracked in '+str(self.elapsed)+'\n\nThe correct word is: '+self.decrypted_hash)
-        
+        self.userHash = self.getHash()
+        self.decryptedHash = self.bruteForceAttack()
+        self.elapsed = (time.time() - self.start)
+        self.myLabel1.config(text='Hash cracked in '+str(self.elapsed)+'\n\nThe correct word is: '+self.decryptedHash)
+
     def myClick1(self):
-        
-        
-         self.myLabel1.place(x=230,y=250)
+        self.myLabel1.place(x=230,y=250)
+        self.myLabel1.config(text='Checking...')
+        self.userHash = self.getHash()
+        self.wordlist = self.readWordlist() # get the wordlist...
+        self.decryptedHash = self.dictionaryAttack()
+        self.elapsed = (time.time() - self.start)
+        self.myLabel1.config(text='Hash cracked in '+str(self.elapsed)+'\n\nThe correct word is: '+self.decryptedHash)
 
-    def main( self ):
 
-       
-        self.user_hash = self.get_hash()
-        decrypted_hash = self.brute_force()
-                
-        if decrypted_hash != None:  
-            self.elapsed = (time.time() - self.start)  
-            self.myLabel1.config(text='Hash cracked in '+str(self.elapsed)+' seconds. The correct word is: '
-                      +decrypted_hash)
-        else:
-            self.myLabel1.config(text='no matches found')
 
-            
-    def get_hash(self):
-         
-        hash_input=self.user_input.get()
-        if hash_input.isalnum(): 
-            length =32
-            if TYPES_DICT.get( length, None ):
-                self.hashtype = TYPES_DICT[length]
-                return hash_input
+
+    def getHash(self):
+        hashInput=self.user_input.get()
+
+        if hashInput.isalnum():
+            length = len(hashInput)
+            if Dictionary_TYPES.get(length, None ):
+                self.hashtype = Dictionary_TYPES[length]
+                return hashInput
 
             else:
                 self.myLabel1.config(text='invalid hash')
-            
+
         else:
             self.myLabel1.config(text='invalid hash')
-                    
-       
-    def brute_force(self):
-        charset = 'helo'
-        minlen=4
-        maxlen=10
+
+
+    def readWordlist(self):
+
+        while self.inputFile == None:
+
+            try:
+                self.inputFile = open("wordlist.txt" , "r")
+
+            except FileNotFoundError:
+                self.retry('file name '+self.filename+'not found')
+
+        words = self.inputFile.read()
+        self.inputFile.close()
+        return words.split()
+
+    def dictionaryAttack(self):
+        self.start = time.time()
+        for word in self.wordlist:
+            temp = self.hashtype(word)
+            if temp == self.userHash:
+                return word
+
+
+    def bruteForceAttack(self):
+        charWord = 'abcedf0123456789'
+        minlen = 3
+        maxlen = 20
         self.start = time.time()
         for i in range(minlen, maxlen+1):
-            for p in product(charset, repeat=i):
-                word = ''.join(p)
-                if self.hashtype(word) == self.user_hash:
+            for j in product(charWord, repeat=i):
+                word = ''.join(j)
+                if self.hashtype(word) == self.userHash:
                     return word
-                           
+
 if __name__ == "__main__":
-    run_it = Control()
-     
+    runIt = Control()
